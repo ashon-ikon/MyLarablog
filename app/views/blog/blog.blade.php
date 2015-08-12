a9d6578953d3d1ae6bcbf13610b68574f5079536
Ashon GigaLimit <gigalimit20@yahoo.com>
2015-08-12 18:47:22 +1000

    + LB0004 Updated the blog controller and added Angular JS implementation to the .blade file. + LB0004 Removed obsolete migration files. + LB0004 Added Doctrine/DBAL to project dependencies

--- app/views/blog/blog.blade.php
+++ app/views/blog/blog.blade.php
@@ -6,19 +6,27 @@
 
 @section('content')
 
-<section>
+<section ng-app="blog-app" ng-controller="blogCtrl">
     @if (count($blogs)) 
-        @foreach($blogs as $blog)
-        <section class="post">
+        @foreach($blogs as $i => $blog)
+        <section class="post" ng-init="blog = blogs[{{$i}}]">
+            <pre>{{ $blog; }}</pre>
             <h3>{{ $blog->title }}</h3>
             <p class="overview">
                 {{ $blog->excerpt }}
             </p>
-            <div class="author"><div>Written by {{ ucwords(strtolower($blog->author[0]->username)) }}</div></div>
+            <div class="author"><div>Written by @{{ blog.author.first_name + ' ' +  blog.author.last_name | lowercase }} </div></div>
+            <div class="tags">@foreach($blog->tags as $tag)<span>{{ ucwords(strtolower($tag->text)) }}</span> @endforeach</div>
         </section>
         @endforeach
     @else
     <p>We DON'T have any content</p>
     @endif
 </section>
+<script type="text/javascript">
+    var blogApp = angular.module('blog-app', []).controller('blogCtrl', function($scope) {
+        $scope.blogs = {{ $blogs->toJson(); }}
+        console.log($scope.blogs);
+    });
+</script>
 @stop
